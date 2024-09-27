9f62eec5c761c95627603410046b5c5069fd7fa0
sohanchatterjee <sohanchatterjee20@gmail.com>
2024-09-27 14:03:22 -0400

    Update TestTriangle.py

--- HW-02/TestTriangle.py
+++ HW-02/TestTriangle.py
@@ -1,10 +1,9 @@
 # -*- coding: utf-8 -*-
 """
-Updated Jan 21, 2018
+Updated Sep 27, 2024
 The primary goal of this file is to demonstrate a simple unittest implementation
 
-@author: jrr
-@author: rk
+@author: Sohan Chatterjee
 """
 
 import unittest
